488dba19ce375f1f4f4fc884415e50ee2bcea549
tsanie <tsorgy@gmail.com>
2018-01-17 23:06:09 +0800

    update test-case

--- test/footnotes.spec.js
+++ test/footnotes.spec.js
@@ -12,13 +12,13 @@ describe('footnotes', function() {
       '<hr>' +
       '<div id="footnotelist">' +
       '<ol style="list-style: none; padding-left: 0; margin-left: 40px">' +
-      '<li id="fn:5">' +
+      '<li id="fn:5" style="list-style: none">' +
       '<span style="display: inline-block; vertical-align: top; padding-right: 10px; margin-left: -40px">' +
       '5.' +
       '</span>' +
-      '<span style="display: inline-block; vertical-align: top; margin-left: 10px;">' +
+      '<span style="display: inline-block; vertical-align: top; margin-left: 10px">' +
       'basic footnote content' +
-      '<a href="#fnref:5" rev="footnote"> ↩</a></span></li></ol></div></div>'
+      '<a href="#fnref:5" rev="footnote" style="margin-left: 6px">&#8617;</a></span></li></ol></div></div>'
     );
   });
 
@@ -30,13 +30,13 @@ describe('footnotes', function() {
       '<hr>' +
       '<div id="footnotelist">' +
       '<ol style="list-style: none; padding-left: 0; margin-left: 40px">' +
-      '<li id="fn:13">' +
+      '<li id="fn:13" style="list-style: none">' +
       '<span style="display: inline-block; vertical-align: top; padding-right: 10px; margin-left: -40px">' +
       '13.' +
       '</span>' +
-      '<span style="display: inline-block; vertical-align: top; margin-left: 10px;">' +
+      '<span style="display: inline-block; vertical-align: top; margin-left: 10px">' +
       'basic footnote content' +
-      '<a href="#fnref:13" rev="footnote"> ↩</a></span></li></ol></div></div>'
+      '<a href="#fnref:13" rev="footnote" style="margin-left: 6px">&#8617;</a></span></li></ol></div></div>'
     );
   });
 
@@ -48,31 +48,31 @@ describe('footnotes', function() {
       '<hr>' +
       '<div id="footnotelist">' +
       '<ol style="list-style: none; padding-left: 0; margin-left: 40px">' +
-      '<li id="fn:2">' +
+      '<li id="fn:2" style="list-style: none">' +
       '<span style="display: inline-block; vertical-align: top; padding-right: 10px; margin-left: -40px">' +
       '2.' +
       '</span>' +
-      '<span style="display: inline-block; vertical-align: top; margin-left: 10px;">' +
+      '<span style="display: inline-block; vertical-align: top; margin-left: 10px">' +
       'friend' +
-      '<a href="#fnref:2" rev="footnote"> ↩</a></span></li></ol></div></div>'
+      '<a href="#fnref:2" rev="footnote" style="margin-left: 6px">&#8617;</a></span></li></ol></div></div>'
     );
   });
 
   it('render (with markdown content)', function() {
     var content = footnotes('hey buddy[^13], it\'s a test [^13]: basic footnote [content](http://example.com)');
     content.should.equal(
-      'hey buddy<sup id="fnref:13"><a href="#fn:13" rel="footnote"><span class="hint--top-right hint--error hint--large" aria-label="basic footnote [content](http://example.com)">[13]</span></a></sup>, it\'s a test ' +
+      'hey buddy<sup id="fnref:13"><a href="#fn:13" rel="footnote"><span class="hint--top-right hint--error hint--large" aria-label="basic footnote content [http://example.com]">[13]</span></a></sup>, it\'s a test ' +
       '<div id="footnotes">' +
       '<hr>' +
       '<div id="footnotelist">' +
       '<ol style="list-style: none; padding-left: 0; margin-left: 40px">' +
-      '<li id="fn:13">' +
+      '<li id="fn:13" style="list-style: none">' +
       '<span style="display: inline-block; vertical-align: top; padding-right: 10px; margin-left: -40px">' +
       '13.' +
       '</span>' +
-      '<span style="display: inline-block; vertical-align: top; margin-left: 10px;">' +
+      '<span style="display: inline-block; vertical-align: top; margin-left: 10px">' +
       'basic footnote <a href="http://example.com">content</a>' +
-      '<a href="#fnref:13" rev="footnote"> ↩</a></span></li></ol></div></div>'
+      '<a href="#fnref:13" rev="footnote" style="margin-left: 6px">&#8617;</a></span></li></ol></div></div>'
     );
   });
 });
