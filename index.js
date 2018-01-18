a56ab554a46c56443f45bef345c57ac67bc6756e
tsanie <tsorgy@gmail.com>
2018-01-18 22:32:57 +0800

    comments

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var renderFootnotes = require('./src/footnotes'),
-    ref = hexo.config.reference,
-    util = require('hexo-util');
+    util = require('hexo-util'),
+    ref = hexo.config.reference;
+
+// Add CDN CSS resources
 var css_link = ref.cdn || 'https://cdn.jsdelivr.net/hint.css/2.4.1/hint.min.css';
 
 // Register footnotes filter
@@ -9,7 +11,6 @@ hexo.extend.filter.register('before_post_render', function(data) {
   return data;
 });
 
-// Add CDN CSS resources
 hexo.extend.filter.register('after_post_render', function(data) {
   data.content =
       util.htmlTag('link', {rel: 'stylesheet', type: 'text/css', href: css_link}) +
